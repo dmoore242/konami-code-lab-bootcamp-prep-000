1a368c4405105f1cd9f754dc592d2e7826f58f38
Devin Moore <devin.moore@volken.org>
2017-12-16 03:00:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-
+var index=0;
 
 function init() {
   // Write your JavaScript code inside the init() function
    document.body.addEventListener('keydown',function(e){
       var key = parseInt(e.which || e.detail);
-      var index=0;
+    
       if(key === code[index]){
       index++
       
